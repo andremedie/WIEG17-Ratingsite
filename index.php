f806d09fdfb34e8e8bfa72a0d27e6208b77070fc
andremedie <32762205+andremedie@users.noreply.github.com>
2018-02-06 12:59:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head><h1> MINA FILMER🐳</h1>
+    <a href="index.php">START</a> <--->     
+    <a href="kontakt.php">Kontakt</a> <--->
+        <a href="list.php">Listan🎥</a></head>
+        <body></body>
+    
+        
+<?php 
+echo "<h3>Hello People!😻</h3> ";
+
+echo "Great Films is found on Information-button!";
+
+
+?>
+<footer><p>Min 👣 ser ut på följande sätt=</p>
+<div>nummer = 0763096174, resten kommer sen.</div>
+</footer>
+</html>
---
+++ kontakt.php
@@ -0,0 +1,17 @@
+
+  <html>
+    <head><h1> MINA FILMER🐳</h1>
+    <a href="index.php">START</a> <--->     
+    <a href="kontakt.php">Kontakt</a> <--->
+        <a href="list.php">Listan🎥</a></head>
+        <body></body>     
+       <?php 
+
+echo "<h1>CONTACT SIDE 🐶</h1>";
+
+echo "<p>My name is André and I live in Göteborg. My favourite color is Green, I love to cook Food, create new Music, and have a great time with my friends and hang out with my girlfriend 👻 </p>";
+?>
+<footer><p>Min 👣 ser ut på följande sätt=</p>
+<div>nummer = 0763096174, resten kommer sen.</div>
+</footer>
+</html>
